6be13f2ff6efecd78c4a365970c6411aca52fea5
Nitesh <niteshsengar9831@gmail.com>
2024-09-24 13:18:37 +0530

    add functionality to login and logout

--- week-6-BasicFBlogin-usinginMemory/backend/index.js
+++ week-6-BasicFBlogin-usinginMemory/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = "iloveyou";
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -23,7 +24,7 @@ function Auth(req,res,next){
         catch(e){
             console.log(e);
             return res.json({
-                message: "Not allwoed to enter the route"
+                message: "Not allowed to enter the route"
             })
         }
 
@@ -69,6 +70,14 @@ app.get('/me',Auth,(req,res)=>{
     
 })
 
+// to avoid complexity of cors we are sending the fronted
+// from backend which means my fronted is also in 3000 port now
+//So no issue of cors
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+//__dirname consist of path of your current directory 
+//you can also do pwd in terminal and past the path here
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
